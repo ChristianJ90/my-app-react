6125d80196cc6b0d22877b7ce2f9aa53466fbd9d
cbarrios <cjbr31121990@gmail.com>
2022-04-05 06:56:52 -0400

    Enlaces activos

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import *as React from 'react';
-import { Routes, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
 import {Navbar,Nav,Container} from 'react-bootstrap';
 import CartWidget from './CartWidget';
 
@@ -7,14 +7,14 @@ export default function NavbarReact() {
 return(
 <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
   <Container>
-  <Nav.Link to="/">Logo</Nav.Link>
+  <Nav.Link as={Link} to="/">Logo</Nav.Link>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="me-auto">
-      <Nav.Link to="/">Inicio</Nav.Link>
-      <Nav.Link to="/Productos">Productos</Nav.Link>
-      <Nav.Link to="/Nosotros">Nosotros</Nav.Link> 
-      <Nav.Link to="Contacto/">Contacto</Nav.Link> 
+      <Nav.Link as={Link} to="/">Inicio</Nav.Link>
+      <Nav.Link as={Link} to="/Productos">Productos</Nav.Link>
+      <Nav.Link as={Link} to="/Nosotros">Nosotros</Nav.Link> 
+      <Nav.Link as={Link} to="Contacto/">Contacto</Nav.Link> 
     </Nav>
     <Nav>
       <Nav.Link eventKey={2} to="/carrito"><CartWidget/>
